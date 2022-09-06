d05a55df9545fd4eb61a976ff4743903a7ebfe2a
Daniel Sucerquia <sucerqdl@rh05297.villa-bosch.de>
2022-09-06 09:18:51 +0200

    ENH: change of the arguments of the ase_increase_distance_file
    now the first argument is the initial structure file
    while the second argument is the name of the .com .chk
    files without extension.

--- ase_increase_distance.py
+++ ase_increase_distance.py
@@ -5,8 +5,8 @@ the parallelization keyword.
 
 python ase_increase_distance.py arg1 arg2 arg3 arg4 [arg5]
 
-arg1: name of the input file without number nor extension.
-arg2: number of the previous calc.
+arg1: name of the input file containing the initial structure.
+arg2: name of the .com .chk files without extension.
 arg3: index of the atom1 to pick and to increase the distance.
 arg4: index of the atom2 to pick and to increase the distance.
 arg5: amount of distance to increase in A. default: 0.5A
@@ -20,8 +20,8 @@ import sys
 
 
 # Read parameters
-file = sys.argv[1]
-i = int(sys.argv[2])
+inp = sys.argv[1]
+out = sys.argv[2]
 index1 = int(sys.argv[3])
 index2 = int(sys.argv[4])
 if len(sys.argv) > 5:
@@ -30,13 +30,13 @@ else:
     deltad = 0.5
 
 # Read previus file
-atoms = read(f'{file}{i}.log')
+atoms = read(inp)
 # Next two lines increase the distance between atoms
 d1norm = atoms.get_distance(index1, index2)
 atoms.set_distance(index1, index2, d1norm+deltad)
 
-calculator = Gaussian(label=f'{file}{i+1}',
-                      chk=f'{file}{i+1}',
+calculator = Gaussian(label=out,
+                      chk=out,
                       xc='bmk',
                       basis='6-31+g')
 
