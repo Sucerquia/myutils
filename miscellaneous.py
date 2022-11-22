b482395bea98d876b99e465058fc16256784dcdb
Daniel Sucerquia <sucerqdl@rh05297.villa-bosch.de>
2022-11-22 15:09:09 +0100

    ADD: minumum energy path respect to a variable
    New function into miscellaneous that divides the ranges of variables into
    subranges and finds the minimim value of the corresponfing y-axis in each
    subrange such that it returns a minimum path.

--- miscellaneous.py
+++ miscellaneous.py
@@ -8,6 +8,137 @@ from matplotlib.transforms import Bbox
 
 
 
+def min_profile(file, indexes=[3 , 2, 0], num_ranges=20):
+    """
+    This function returns the profile of minimum potential energy respect to
+    one variable.
+
+    Parameters
+    ----------
+
+    file: string
+        file that contains the data.
+
+    indexes: list of ints
+        indexes of the columns that contains the data of variable, energy and
+        time. Default indexes are 3, 2, 0 that corresponds to the distance
+        variable, pot energy and time in the file analysis_merged_table.dat
+
+    num_ranges: int
+        number of blocks to divide the variable range. Default 20
+
+    Note: The idea of this function is to split the variable in ranges and to 
+    take the minimum energy in each range.
+
+    Return
+    ------
+        Duple with de data time, variable, energy
+    """
+
+    variables, energies, times = np.loadtxt(file,
+                                            usecols=[3, 2, 0],
+                                            unpack=True)
+    subranges = np.linspace(min(variables),
+                            max(variables),
+                            num_ranges)
+
+    split_var = []
+    split_ener = []
+    split_time = []
+
+    for index in range(len(subranges[:-1])):
+        split_var.append(variables[np.logical_and(variables >= subranges[index],
+                                                variables < subranges[index+1])])
+        split_ener.append(energies[np.logical_and(variables >= subranges[index],
+                                                variables < subranges[index+1])])
+        split_time.append(times[np.logical_and(variables >= subranges[index],
+                                            variables < subranges[index+1])])
+
+    var = [variables[0]]
+    ener = [energies[0]]
+    time = [times[0]]
+
+    for i in range(len(split_var)):
+        try:
+            index = np.where(split_ener[i] == min(split_ener[i]))[0][0]
+            var.append(split_var[i][index])
+            ener.append(split_ener[i][index])
+            time.append(split_time[i][index])
+        except:
+            continue
+
+    return time, var, ener
+
+def min_profile_from_several(files, indexes=[3 , 2, 0], num_ranges=20):
+    """
+    This function returns the profile of minimum potential energy respect to
+    one variable.
+
+    Parameters
+    ----------
+
+    files: list of strings
+        files that contain the data.
+
+    indexes: list of ints
+        indexes of the columns that contains the data of variable, energy and
+        time. Default indexes are 3, 2, 0 that corresponds to the distance
+        variable, pot energy and time in the file analysis_merged_table.dat
+
+    num_ranges: int
+        number of blocks to divide the variable range. Default 20
+
+    Note: The idea of this function is to split the variable in ranges and to 
+    take the minimum energy in each range.
+
+    Return
+    ------
+        Duple with de data time, variable, energy
+    """
+
+    variables = []
+    energies = []
+    times = []
+    for file in files:
+        variable, energy, time = np.loadtxt(file,
+                                            usecols=[3, 2, 0],
+                                            unpack=True)
+        variables = np.append(variable, variables)
+        energies = np.append(energy, energies)
+        times = np.append(time, times)
+
+    subranges = np.linspace(min(variables),
+                            max(variables),
+                            num_ranges)
+
+    split_var = []
+    split_ener = []
+    split_time = []
+
+    for index in range(len(subranges[:-1])):
+        split_var.append(variables[np.logical_and(variables >= subranges[index],
+                                                variables < subranges[index+1])])
+        split_ener.append(energies[np.logical_and(variables >= subranges[index],
+                                                variables < subranges[index+1])])
+        split_time.append(times[np.logical_and(variables >= subranges[index],
+                                            variables < subranges[index+1])])
+
+    var = [variables[0]]
+    ener = [energies[0]]
+    time = [times[0]]
+
+    for i in range(len(split_var)):
+        try:
+            index = np.where(split_ener[i] == min(split_ener[i]))[0][0]
+            var.append(split_var[i][index])
+            ener.append(split_ener[i][index])
+            time.append(split_time[i][index])
+        except:
+            continue
+
+    return time, var, ener
+
+
 def plot_sith(dofs, xlabel, energy_units='a.u', fig=None, ax=None, cbar=True,
               cmap=cmo.cm.algae, orientation='vertical', labelsize=15,
               axes=None, aspect=25):
