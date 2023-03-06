11fe02e70aa0fedebe5a8cf8bfed939865fb4867
Daniel Sucerquia <sucerqdl@rh05297.villa-bosch.de>
2023-03-06 18:09:34 +0100

    ADD: function that optimizes a molecule from a pdb file using a classical forcefield.

---
+++ src/myutils/gromacs/classical_minimization.sh
@@ -0,0 +1,51 @@
+print_help () {
+echo "
+This tool optimizes a configuration from an initial pdb file"
+}
+
+while getopts 'h' flag; do
+    case "${flag}" in
+        h) print_help
+done
+fail () {
+    printf '%s\n' "$1" >&2
+    printf '%s\n' "$1"
+    exit "${2-1}"
+}
+
+pdbfile=$1
+
+echo "
+    ++++++++ CLASSICALMIN_MSG: VERBOSE - creating .gro file from $pdbfile ++++++++++++++++++"
+echo -e "4\n 7\n" | gmx pdb2gmx -f $pdbfile -o minim.gro -ignh >&2 || fail "
+    ++++++++ CLASSICALMIN_MSG: ERROR - creating gro file +++++++++++++++++++++"
+
+gmx editconf -f minim.gro \
+             -o minim_box.gro \
+             -c -d 5.0 -bt cubic >&2 || fail "
+    ++++++++ CLASSICALMIN_MSG: ERROR - creating simulation box +++++++++++++++"
+
+mv minim_box.gro minim.gro
+
+gmx grompp -f $( myutils minim ) \
+           -c minim.gro \
+           -p topol.top \
+           -o em.tpr  \
+           -maxwarn 10 >&2 || fail "
+    ++++++++ CLASSICALMIN_MSG: ERROR - creating em +++++++++++++++++++++++++++"
+
+echo "
+    ++++++++ CLASSICALMIN_MSG: VERBOSE - run minimization ++++++++++++++++++++"
+gmx mdrun -v -deffnm em >&2 || fail "
+    ++++++++ CLASSICALMIN_MSG: ERROR - computing energy ++++++++++++++++++++++"
+
+echo -e "0\n" | gmx trjconv -f em.gro -o $pdbfile -s em.tpr >&2 \
+    || fail "
+    ++++++++ CLASSICALMIN_MSG: ERROR - extracting pdb ++++++++++++++++++++++++"
+
+rm \#*
+rm em.*
+rm mdout.mdp
+rm mini*
+rm posre.itp
+rm topol.top
