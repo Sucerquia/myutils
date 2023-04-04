0ffef7f4c4d4fff0fa368fbcdf1bd3e50aecf142
Daniel Sucerquia <sucerqdl@mbm.am>
2023-04-04 16:19:04 +0200

    BUG: xyz2pdb fixed to consider those pdbs that contains X column

--- src/myutils/sith/xyz2pdb.py
+++ src/myutils/sith/xyz2pdb.py
@@ -2,7 +2,8 @@ from ase.io import read
 import glob
 
 
-def transform(xyzfile, pdbtemplate, pdbfile=None):
+# add2executable
+def xyz2pdb(xyzfile, pdbtemplate, pdbfile=None, withx=0):
     if pdbfile is None:
         pdbfile = xyzfile.split('.')[0] + '.pdb'
     atoms = read(xyzfile)
@@ -10,12 +11,14 @@ def transform(xyzfile, pdbtemplate, pdbfile=None):
     with open(pdbtemplate) as f:
         lines = f.readlines()
 
+    i_atom = 0
     for index in range(len(lines)):
         line_split = lines[index].split()
         if 'ATOM' in line_split[0]:
-            atomposition = positions[index]
+            atomposition = positions[i_atom]
+            i_atom += 1
             for i in [0, 1, 2]:
-                toreplace = line_split[6+i]
+                toreplace = line_split[6+withx+i]
                 n2dot_toreplace = len(toreplace.split('.')[0])
                 replacefor = "{:.3f}".format(atomposition[i])
                 n2dot_replacefor = len(replacefor.split('.')[0])
