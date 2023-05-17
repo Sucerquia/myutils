cffb316b16b7189d5a4aa9ef97dd62aa5bbf639f
Daniel Sucerquia <sucerqdl@mbm.am>
2023-05-17 17:23:41 +0200

    TEST: test of gromacs pulling and analysis added
    Still the test regarding the other gromacs scripts are not created.

---
+++ src/myutils/tests/gromacs/test_analysis.py
@@ -0,0 +1,36 @@
+import numpy as np
+from myutils.miscellaneous import output_terminal
+from ase.io import read
+from pathlib import Path
+from pytest import approx
+
+
+def test_output():
+    u = output_terminal("$(myutils peptide_pulling) -p GPA -a '' -f '100 300' -s 501")
+    assert 'process finished successfully **' in u
+    assert 'Pulling of GPA starts' in u
+    assert 'VERBOSE Force 100 acting GPA starts' in u
+    assert 'VERBOSE Force 300 acting GPA starts' in u
+    assert 'Pulling finished correctly of 100' in u
+    assert 'Pulling finished correctly of 300' in u
+    assert 'GPA pulling finishes' in u
+
+def test_analysis():
+    output_terminal("cd force0100 ; $(myutils analysis) -a -m -f md_0_0100")
+    with open('force0100/analysis_merged_table-md_0_0100.dat', 'r') as an:
+        total_lines = an.readlines()
+        head=total_lines[0]
+        first_line=total_lines[1]
+    values = first_line.split()
+    float(values[1])
+    float(values[2])
+    float(values[3])
+    assert 'time' in head
+    assert 'e_potential' in head
+    assert 'e_pot_pep' in head
+    assert 'distance' in head
+    assert float(values[0]) == approx(0)
+
+def test_remove():
+    output_terminal('rm -r force* equilibrate')
+    assert True
