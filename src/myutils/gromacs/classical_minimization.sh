8de911f4e275cdc7a4d74c1dd5acbd388877c90a
Sucerquia <danielsucerquia@hotmail.com>
2023-05-22 16:47:26 +0200

    ENH: gromacs.classical_minimization uses basic bash functions

--- src/myutils/gromacs/classical_minimization.sh
+++ src/myutils/gromacs/classical_minimization.sh
@@ -2,29 +2,43 @@ source $(myutils basics) CLASSICAL_MIN
 
 print_help () {
 echo "
-This tool optimizes a configuration from an initial pdb file"
+This tool optimizes a configuration from an initial pdb file, which must be the
+first argument.
+
+   -i   pdb file with the configuration to be minimized.
+   -o   name of the output file with the optimized structure. Default same
+        input (replaces the pdb of the input).
+   -l   log file of the gromacs outputs. Default /dev/null
+
+   -h   prints this message.
+"
+exit 0
 }
 
-while getopts 'h' flag; do
+output='/dev/null'
+while getopts 'i:o:l:h' flag; do
     case "${flag}" in
+        i) pdbfile=${OPTARG} ;;
+        o) output_file=${OPTARG} ;;
+        l) output=${OPTARG} ;;
+
         h) print_help
     esac
 done
-fail () {
-    printf '%s\n' "$1" >&2
-    printf '%s\n' "$1"
-    exit "${2-1}"
-}
 
-pdbfile=$1
+if [ ${#output_file} -eq 0 ]
+then
+    output_file=$pdbfile
+fi
 
 verbose "creating .gro file from $pdbfile"
-echo -e "4\n 7\n" | gmx pdb2gmx -f $pdbfile -o minim.gro -ignh || fail "
-    creating gro file"
+echo -e "4\n 7\n" | gmx pdb2gmx -f $pdbfile -o minim.gro -ignh > $output 2>&1\
+    || fail "Creating gro file"
 
 gmx editconf -f minim.gro \
              -o minim_box.gro \
-             -c -d 5.0 -bt cubic || fail "creating simulation box"
+             -c -d 5.0 -bt cubic > $output 2>&1 || \
+    fail "creating simulation box"
 
 mv minim_box.gro minim.gro
 
@@ -32,13 +46,13 @@ gmx grompp -f $( myutils minim ) \
            -c minim.gro \
            -p topol.top \
            -o em.tpr  \
-           -maxwarn 10 || fail "creating em"
+           -maxwarn 10 > $output 2>&1 || fail "creating em"
 
 verbose "run minimization"
-gmx mdrun -v -deffnm em || fail "computing energy"
+gmx mdrun -v -deffnm em > $output 2>&1 || fail "computing energy"
 
-echo -e "0\n" | gmx trjconv -f em.gro -o $pdbfile -s em.tpr >&2 \
-    || fail "extracting pdb"
+echo -e "0\n" | gmx trjconv -f em.gro -o $output_file -s em.tpr > \
+    $output 2>&1 || fail "extracting pdb"
 
 
 rm \#*
