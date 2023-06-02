53989097c69f6568145eecfc3655664ffe602737
Sucerquia <dsucerg@gmail.com>
2023-06-02 16:27:23 +0200

    TST: new functions of ase tools tested
    These are the test of the functions moved from myutils.analysis to myutils.ase_utils.tools

--- src/myutils/tests/ase_utils/test_tools.py
+++ src/myutils/tests/ase_utils/test_tools.py
@@ -1,14 +1,31 @@
 from myutils.ase_utils.tools import MoleculeSetter
-from myutils.ase_utils.tools import xyz2pdb
+from myutils.ase_utils.tools import (extract_bonds,
+                                     diff_bonds,
+                                     conf2pdb,
+                                     all_hydrogen_atoms,
+                                     distance)
 from myutils.tests.variables4tests import (gpa_endo_xyz,
                                            gpa_opti_pdb,
                                            gpa_atoms,
-                                           gpa_residues)
+                                           gpa_residues,
+                                           gpa_bonds,
+                                           gpa_broken_xyz)
 from myutils.miscellaneous import output_terminal
 from pytest import approx
 from ase.io import read
 from ase import Atoms
 import numpy as np
+
+
+def test_extract_bonds():
+    bonds = extract_bonds(gpa_endo_xyz)
+    assert bonds == gpa_bonds
+
+
+def test_diff_bonds():
+    bkn_bonds = diff_bonds(gpa_endo_xyz, gpa_broken_xyz,
+                           frozen_dofs='remove_dofs.dat')
+    assert bkn_bonds == [[1, 5]]
 
 
 def test_conf2pdb():
@@ -19,6 +36,19 @@ def test_conf2pdb():
     assert atoms.get_chemical_symbols() == refer.get_chemical_symbols()
     assert (atoms.arrays['atomtypes'] == gpa_atoms).all()
     assert (atoms.arrays['residuenames'] == gpa_residues).all()
+
+
+def test_all_hydrogen_atoms():
+    atoms = read(gpa_endo_xyz)
+    indexes = all_hydrogen_atoms(atoms) - 1
+    h22 = atoms[indexes]
+    assert h22.get_chemical_formula() == 'H22'
+
+
+def test_distance():
+    # values of reference obtained with vmd
+    assert distance(gpa_broken_xyz, 0, 39) == approx(17.549, rel=1e-4)
+    assert distance(gpa_endo_xyz, 0, 39) == approx(9.749, rel=1e-4)
 
 
 def initialize_ms():
--- src/myutils/tests/variables4tests.py
+++ src/myutils/tests/variables4tests.py
@@ -8,6 +8,7 @@ gpa_exo_pdb = str(Path(__file__).parent) + "/references/GPA-exo.pdb"
 gpa_endo_xyz = str(Path(__file__).parent) + "/references/GPA-endo.xyz"
 gpa_opti_pdb = str(Path(__file__).parent) +  "/references/GPA-stretched00.pdb"
 gpa_stre_pdb = str(Path(__file__).parent) +  "/references/GPA-stretched30.pdb"
+gpa_broken_xyz = str(Path(__file__).parent) +  "/references/GPA-broken.xyz"
 frozendofs_dat = str(Path(__file__).parent) +  "/references/frozen_dofs.dat"
 
 gpa_atoms = np.array(['CH3', '1HH3', '2HH3', '3HH3', 'C', 'O', 'N', 'H', 'CA',
@@ -23,3 +24,11 @@ gpa_residues = np.array(['ACE ', 'ACE ', 'ACE ', 'ACE ', 'ACE ', 'ACE ', 'GLY ',
                          'ALA ', 'ALA ', 'ALA ', 'ALA ', 'ALA ', 'ALA ', 'ALA ',
                          'ALA ', 'ALA ', 'NME ', 'NME ', 'NME ', 'NME ', 'NME ',
                          'NME '])
+
+gpa_bonds = [[1, 2], [1, 3], [1, 4], [1, 5], [5, 6], [5, 7], [7, 8], [7, 9],
+             [9, 10], [9, 11], [9, 12], [12, 13], [12, 14], [14, 15], [14, 24],
+             [15, 16], [15, 17], [15, 18], [18, 19], [18, 20], [18, 21],
+             [21, 22], [21, 23], [21, 24], [24, 25], [24, 26], [26, 27],
+             [26, 28], [28, 29], [28, 30], [30, 31], [30, 32], [30, 36],
+             [32, 33], [32, 34], [32, 35], [36, 37], [36, 38], [38, 39],
+             [38, 40], [40, 41], [40, 42], [40, 43]]
