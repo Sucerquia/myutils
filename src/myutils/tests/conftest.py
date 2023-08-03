5cc51166c0f43eeea7b182b5391fc262f87915ee
Sucerquia <dsucerg@gmail.com>
2023-08-03 20:36:47 +0200

    BUG: delete foces0100 instead forces100

--- src/myutils/tests/conftest.py
+++ src/myutils/tests/conftest.py
@@ -6,7 +6,7 @@ from myutils.miscellaneous import output_terminal
 # test.
 def remove_created_files():
     print("\n TESTS END: removing created files and directories.")
-    output_terminal('rm -rf equilibrate force100 index.ndx md_0_0100.tpr' +
+    output_terminal('rm -rf equilibrate force0100 index.ndx md_0_0100.tpr' +
                     ' pulling.mdp')
     output_terminal('find $path -name "remove*"' +
                     ' -exec rm -r {} +;')
