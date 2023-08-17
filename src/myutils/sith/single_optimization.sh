c59a456ff095f44cfc15b8db421c924360845ea4
Sucerquia <dsucerg@gmail.com>
2023-08-17 11:48:37 +0200

    ENH: single optimization with bash convention

--- src/myutils/sith/single_optimization.sh
+++ src/myutils/sith/single_optimization.sh
@@ -8,26 +8,44 @@
 #SBATCH --error=optimization-%j.e
 #SBATCH --exclusive
 
-if [ "$1" == '-h' ]
+
+print_help() {
+echo "
+This code runs one optimization using gaussian in one of the clusters. You
+have to create the input file and give it (without .com extension) as first
+argument when run this code.
+
+    -c    run in cascade.
+
+    -h    prints this message.
+"
+exit 0
+}
+
+cascade='false'
+while getopts 'd:cp:h' flag; do
+    case "${flag}" in
+      c) cascade='true' ;;
+
+      h) print_help ;;
+      *) echo "for usage check: myutils <function> -h" >&2 ; exit 1 ;;
+    esac
+done
+
+if $cascade
 then
-    echo "
-    This code runs one optimization using gaussian in one of the clusters. You
-    have to create the input file and give it (without .com extension) as first
-    argument when run this code."
-    exit 0
+    echo "This JOB will be run in the Node:"
+    echo "$SLURM_JOB_NODELIST"
+    cd "$SLURM_SUBMIT_DIR" || fail "moving to the directory from where the job was 
+        submitted $SLURM_SUBMIT_DIR"
+
+    module purge
+
+    source "$HOME/.bashrc"
+    # shellcheck disable=SC1091
+    source /etc/profile.d/modules.sh
+    # shellcheck disable=SC1091
+    source /hits/basement/mbm/sucerquia/exec/load_g09.sh
 fi
-
-echo "This JOB will be run in the Node:"
-echo "$SLURM_JOB_NODELIST"
-cd "$SLURM_SUBMIT_DIR" || fail "moving to the directory from where the job was 
-    submitted $SLURM_SUBMIT_DIR"
-
-module purge
-
-source "$HOME/.bashrc"
-# shellcheck disable=SC1091
-source /etc/profile.d/modules.sh
-# shellcheck disable=SC1091
-source /hits/basement/mbm/sucerquia/exec/load_g09.sh
 
 g09 "$1.com" "$1.log"
