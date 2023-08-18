692dc1e31340e377a77d424a52c29d0c910030cd
Sucerquia <dsucerg@gmail.com>
2023-08-18 15:34:31 +0200

    ENH: removing non-desired files after tests

--- src/myutils/tests/conftest.py
+++ src/myutils/tests/conftest.py
@@ -5,14 +5,26 @@ from myutils.miscellaneous import output_terminal
 # the next function deletes all those files and directories created duting the
 # test.
 def remove_created_files():
+    files2remove = ["index",
+                    "md*.tpr",
+                    "md*.mdp",
+                    "pulling.mdp",
+                    "minim.gro",
+                    "posre.itp",
+                    "topol.top",
+                    "em.*",
+                    "classical_energy.dat"]
+    directories2remove = ["equilibrate",
+                          "force"]
+    
     print("\n TESTS END: removing created files and directories.")
-    output_terminal('rm -rf equilibrate force* index.ndx md_*'
-                    'rm -rf gromacs/equilibrate gromacs/force* '
-                    'gromacs/index.ndx gromacs/md_* pulling.mdp')
-    output_terminal('find $path -type f -name "remove*"'
-                    ' -exec rm {} +;')
-    output_terminal('find $path -type d -name "remove*"'
-                    ' -exec rm -r {} +;')
+    for file in files2remove:
+        output_terminal(f'find . -type f -name "{file}" -exec rm ' + '{} +')
+    for dir in directories2remove:
+        output_terminal(f'find . -type f -name "{dir}" -exec rm -rf ' + '{} +')
+    output_terminal('find . -type f -name "remove*" -exec rm {} +;')
+    output_terminal('find . -type d -name "remove*" -exec rm -r {} +;')
+    
 
 
 def pytest_unconfigure(config):
