ff979bb7473a740fc62e88f42d1efe1a4d012da9
Sucerquia <dsucerg@gmail.com>
2023-12-05 04:57:24 +0100

    CHG: renaming sith variables as convention

--- src/myutils/sith/sith.py
+++ src/myutils/sith/sith.py
@@ -24,11 +24,11 @@ class Geometry:
         self.nAtoms = len(self.atoms)
         indexes = np.loadtxt(force_file, usecols=1,
                              dtype=str)
-        self.dimIndices = [eval(ric_indexes) for ric_indexes in indexes]
-        self.ric = np.loadtxt(force_file, usecols=3)
+        self.dim_indices = [eval(ric_indexes) for ric_indexes in indexes]
+        self.dof = np.loadtxt(force_file, usecols=3)
 
         self.dims = np.zeros((4), dtype=int)
-        for index in self.dimIndices:
+        for index in self.dim_indices:
             self.dims[0] += 1
             self.dims[len(index) - 1] += 1
 
@@ -38,7 +38,7 @@ class Geometry:
 
         with open(force_file) as ff:
             header = ff.readline()
-        self.energy = float(header.replace('\n', '').split(' ')[-1])
+        self.scf_energy = float(header.replace('\n', '').split(' ')[-1])
 
     def _killDOFs(self, dofis):
         """
@@ -54,14 +54,14 @@ class Geometry:
         ======
         (tuple) [DIFs, dims, intertanl_forces] removing the desired DOFs.
         """
-        self.ric = np.delete(self.ric, dofis)
+        self.dof = np.delete(self.dof, dofis)
         # counter of the number of DOF removed arranged in
         # types (lenght, angle, dihedral)
         tdofremoved = [0, 0, 0]
 
         for index in sorted(dofis, reverse=True):
-            tdofremoved[len(self.dimIndices[index]) - 2] += 1
-            del self.dimIndices[index]
+            tdofremoved[len(self.dim_indices[index]) - 2] += 1
+            del self.dim_indices[index]
 
         self.dims[0] -= len(dofis)
         self.dims[1] -= tdofremoved[0]
@@ -70,7 +70,7 @@ class Geometry:
         # change for forces
         if (self.internal_forces is not None):
             self.internal_forces = np.delete(self.internal_forces, dofis)
-        return self.ric, self.dims, self.internal_forces
+        return self.dof, self.dims, self.internal_forces
 
 
 class Sith:
@@ -126,7 +126,7 @@ class Sith:
         self.setting_force_xyz_files(forces_xyz_files, master_directory)
 
         # create Geometries shape=(n_def, 1)
-        self._deformed = [Geometry(i, j)
+        self.structures = [Geometry(i, j)
                           for i, j in zip(self.xyz_files, self.forces_files)]
 
         # remove atoms, dofs and last or first configurations
@@ -138,24 +138,24 @@ class Sith:
             killElements = []
 
         # number of deformed configs
-        self.n_deformed = len(self._deformed)
-        self.dims = self._deformed[0].dims
+        self.nstructures = len(self.structures)
+        self.dims = self.structures[0].dims
 
         # debug: test the DOFs in all force files
         self._check_dofs()
 
         # Create matrices
         # # DFT energies for each configuration shape=(n_def, 1)
-        self.scf_energy = np.array([defo.energy - self._deformed[0].energy
-                                    for defo in self._deformed])
+        self.scf_energy = np.array([defo.energy - self.structures[0].scf_energy
+                                    for defo in self.structures])
         # # ric values matrix shape=(n_def, n_dofs)
-        self.qF = np.array([defo.ric for defo in self._deformed])
-        self.all_rics = self.rics()
+        self.qF = np.array([defo.ric for defo in self.structures])
+        self.all_rics = self.dofs()
         # # matrix changes shape=(n_def, n_dofs)
         self.deltaQ = self.extract_changes()
         # # all forces shape=(n_def, n_dofs)
         self.all_forces = np.array([defo.internal_forces
-                                    for defo in self._deformed])
+                                    for defo in self.structures])
 
         # Numerical integration
         # # energies per DOF shape=(n_def, n_dofs)
@@ -256,9 +256,9 @@ class Sith:
         (bool) [True] In case it is successful. If it fails, it raises an
         assert error.
         """
-        referece = self._deformed[0].dimIndices
-        for defo in self._deformed[1:]:
-            to_compare = defo.dimIndices
+        referece = self.structures[0].dim_indices
+        for defo in self.structures[1:]:
+            to_compare = defo.dim_indices
             assert len(to_compare) == len(referece), \
                 "The number of DOFs in the first force file and in " + \
                 f"{defo.name} is different."
@@ -280,7 +280,7 @@ class Sith:
         configuration.
         """
         rics = list()
-        for defo in self._deformed:
+        for defo in self.structures:
             ric = defo.ric.copy()
             ric[defo.dims[1]:] = ric[defo.dims[1]:] * np.pi / 180
             rics.append(ric)
@@ -356,7 +356,7 @@ class Sith:
         rics = self.all_rics.copy().T
 
         for angle in rics[self.dims[1]:]:
-            for i in range(self.n_deformed - 1):
+            for i in range(self.nstructures - 1):
                 while angle[i + 1] - angle[i] > np.pi:
                     angle[i + 1:] -= 2 * np.pi
                 while angle[i + 1] - angle[i] < -np.pi:
@@ -368,7 +368,7 @@ class Sith:
         all_ener = np.array([[0] * self.dims[0]])
         # next loop is a 'nasty' cummulative integration. Maybe it could
         # be improved
-        for i in range(1, self.n_deformed):
+        for i in range(1, self.nstructures):
             ener_def = -simpson(self.all_forces[: i + 1],
                                 x=rics[: i + 1],
                                 axis=0)
@@ -416,11 +416,11 @@ class Sith:
         (list) [int] indexes of the removed DOFs.
         """
 
-        self.dims_to_kill = [self._deformed[0].dimIndices[i] for i in killDOFs]
+        self.dims_to_kill = [self.structures[0].dim_indices[i] for i in killDOFs]
         self.atoms_to_kill = killAtoms
 
         # concatenate elements in atoms to be killed
-        molecule = np.array(self._deformed[0].atoms.get_chemical_symbols())
+        molecule = np.array(self.structures[0].atoms.get_chemical_symbols())
 
         for element in killElements:
             indexes_element = np.where(molecule == element)[0] + 1
@@ -429,7 +429,7 @@ class Sith:
         # concatenate atoms in DOFs to be killed
         for atom in self.atoms_to_kill:
             self.dims_to_kill.extend(
-                [dim for dim in self._deformed[0].dimIndices if atom in dim])
+                [dim for dim in self.structures[0].dim_indices if atom in dim])
         # remove repetitions
         self.dims_to_kill = list(dict.fromkeys(self.dims_to_kill))
 
@@ -441,11 +441,11 @@ class Sith:
     def __killDOFs(self, dofs):
         rIndices = list()
         for dof in dofs:
-            rIndices.extend([i for i in range(self._deformed[0].dims[0])
-                            if self._deformed[0].dimIndices[i] == dof])
+            rIndices.extend([i for i in range(self.structures[0].dims[0])
+                            if self.structures[0].dim_indices[i] == dof])
 
         # kill DOFs in Geometries
-        for defo in self._deformed:
+        for defo in self.structures:
             defo._killDOFs(rIndices)
 
         # kill DOFs in sith
@@ -474,11 +474,11 @@ class Sith:
 
         Return
         ======
-        (list) Deformed Geometry objects. self._deformed.
+        (list) Deformed Geometry objects. self.structures.
         """
         ini_index = rem_first_def
-        last_index = self.n_deformed - rem_last_def
-        self._deformed = self._deformed[ini_index: last_index]
+        last_index = self.nstructures - rem_last_def
+        self.structures = self.structures[ini_index: last_index]
         self.qF = self.qF[ini_index: last_index]
         self.deltaQ = self.deltaQ[ini_index: last_index]
         self.all_forces = self.all_forces[ini_index: last_index]
@@ -489,6 +489,6 @@ class Sith:
         self.scf_energy = self.scf_energy[ini_index: last_index] - \
             self.scf_energy[ini_index]
         self.all_rics = self.all_rics[ini_index: last_index]
-        self.n_deformed -= rem_first_def + rem_last_def
+        self.nstructures -= rem_first_def + rem_last_def
 
-        return self._deformed
+        return self.structures
