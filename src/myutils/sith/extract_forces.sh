0fce2aa91cb4242c4f73abc83e8f846afefca905
Sucerquia <danielsucerquia@hotmail.com>
2024-01-29 11:43:57 +0100

    ENH: extract forces in sith convention format

--- src/myutils/sith/extract_forces.sh
+++ src/myutils/sith/extract_forces.sh
@@ -81,10 +81,11 @@ for file in "${log_files[@]}"
 do
     # same name than the log file but with fchk extension
     output=${file%.*}.fchk
-    echo "Forces extraxted from log files" > $output
-
+    echo "Forces extracted from log files" > $output
+    
     # region AtomicNumbers_n_coords
-    number=$( grep -n "Center     Atomic      Atomic" "$file" | cut -d ":" -f 1 )
+    number=$( grep -n "Center     Atomic      Atomic" "$file" \
+        | tail -n 1 | cut -d ":" -f 1 )
     awk -v num=$(( number + 3 )) 'NR >= num { print $0 }' "$file" > tmp1.txt
 
     # find the end of the block of the internal forces
@@ -94,7 +95,6 @@ do
     # store atomic numbers in an array
     mapfile -t atomic_nums < <(awk '{ print $2 }' tmp2.txt)
     mapfile -t coords < <(awk '{ printf "%f \n %f \n %f \n", $4, $5, $6 }' tmp2.txt)
-    
 
     # write atomic numbers in the file
     line=$(printf "%-43s" "Atomic numbers")
@@ -111,10 +111,75 @@ do
     write_float_vector "${coords[@]}" >> $output
     # endregion
 
-    # region internal_forces-dofsindexes
+    # region dofs_indexes
     # find the begining of the block of the internal forces
     number=$( grep -n "Internal Coordinate Forces" "$file" | cut -d ":" -f 1 )
     awk -v num=$(( number + 3 )) 'NR >= num { print $0 }' "$file" > tmp1.txt
+    number=$( grep -n "\-\-\-\-\-\-\-\-" tmp1.txt| head -n 1 | cut -d ":" -f 1 )
+    head -n $(( number - 1 )) tmp1.txt > tmp2.txt
+    sed -i "s/)//g ; s/(//g"  tmp2.txt
+    dist=$(awk 'BEGIN{count=0;}{if( $3 ){count+=1}}END{print count}' tmp2.txt)
+    angl=$(awk 'BEGIN{count=0;}{if( $6 ){count+=1}}END{print count}' tmp2.txt)
+    dihe=$(awk 'BEGIN{count=0;}{if( $9 ){count+=1}}END{print count}' tmp2.txt)
+    ndof=$(( dist + angl + dihe ))
+    dim=( $ndof $dist $angl $dihe )
 
+    # write dof dimensions in the file
+    line=$(printf "%-43s" "Redundant internal dimensions")
+    line+="I   N="
+    line+=$(printf "%12s" "${#dim[@]}")
+    echo "$line" >> $output
+    write_int_vector "${dim[@]}" >> $output
+    # endregion
+
+    # region dofs_indexes
+    # find the begining of the block of the internal forces
+    awk '{if( $3 ){ printf "0\n0\n%d\n%d\n", $1, $3 }}' tmp2.txt > tmp1.txt
+    awk '{if( $6 ){ printf "0\n%d\n%d\n%d\n", $1, $3, $6 }}' tmp2.txt >> tmp1.txt
+    awk '{if( $9 ){ printf "%d\n%d\n%d\n%d\n", $1, $3, $6, $9 }}' tmp2.txt >> tmp1.txt
+
+    mapfile -t indexes < tmp1.txt
+
+    # write indices of internal coordinates in the file
+    line=$(printf "%-43s" "Redundant internal coordinate indices")
+    line+="I   N="
+    line+=$(printf "%12s" "${#indexes[@]}")
+    echo "$line" >> $output
+    write_int_vector "${indexes[@]}" >> $output
+    # endregion
+
+    # region forces
+    awk '{if( $3 ){ printf "%f\n", $4 }}' tmp2.txt > tmp1.txt
+    awk '{if( $6 ){ printf "%f\n", $7 }}' tmp2.txt >> tmp1.txt
+    awk '{if( $9 ){ printf "%f\n", $9 }}' tmp2.txt >> tmp1.txt
+
+    mapfile -t forces < tmp1.txt
+
+    # write indices of internal coordinates in the file
+    line=$(printf "%-43s" "Internal Forces")
+    line+="R   N="
+    line+=$(printf "%12s" "${#forces[@]}")
+    echo "$line" >> $output
+    write_float_vector "${forces[@]}" >> $output
+    # endregion
+
+    # region energy
+    ener=$(grep "SCF Done:" $file | \
+        tail -n 1 | awk '{print $5}')
+    line=$(printf "%-43s" "Total Energy")
+    line+="R"
+    line+=$(printf "%27s" "$ener")
+    echo "$line" >> $output
+    # endregion
+
+    # region dofs_values
+    head=$( grep -n "Variables:" "$file" | cut -d ":" -f 1 )
+    end=$( tail -n +$(( head + 1 )) "$file" | grep -n "^ $" | head -n 1 | cut -d ":" -f 1 )
+    mapfile -t dof_val < <(tail -n +$(( head + 1 )) "$file" | head -n $(( end - 1 )) | awk '{print $2}')
+    line=$(printf "%-43s" "Redundant internal coordinates")
+    line+="R   N="
+    line+=$(printf "%12s" "${#dof_val[@]}")
+    echo "$line" >> $output
+    write_float_vector "${dof_val[@]}" >> $output
     # endregion
 done
