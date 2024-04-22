a5147b03f9debab4361d0e713ec7f7b0af1aca74
Sucerquia <dsucerg@gmail.com>
2024-04-22 17:52:56 +0200

    ENH: updated sith analysis with SITH_bc

--- src/myutils/sith/analysis.py
+++ src/myutils/sith/analysis.py
@@ -2,9 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from myutils.plotters import StandardPlotter
 from SITH.SITH import SITH
-from myutils.miscellaneous import output_terminal
 from myutils.peptides import PepSetter
-from os.path import isdir
+from pathlib import Path
+from typing import Union, Callable
 
 
 def dof_classificator_all(dofs_indexes, atoms_per_aminoacids):
@@ -161,7 +161,6 @@ def le_same_aminoacids(sith, peptides_info, atom_types, kind_amino):
     return all_le
 
 
-# TODO: deprecated, up to date with last version of sith.
 class SithAnalysis:
     def __init__(self, sith, pepinfo):
         self.sith = sith
@@ -186,7 +185,7 @@ class SithAnalysis:
         dof_e = self.sith.dofs_energies[:, dof_i]
         dof_e -= dof_e[0]
 
-        return dof_value - dof_value[0], dof_e
+        return dof_value, dof_e
 
     def index_dof(self, target: tuple):
         """
@@ -210,67 +209,6 @@ class SithAnalysis:
                 return i
         raise ValueError("Non-found dof.")
 
-
-class DataSetAnalysis:
-    def __init__(self, data_dir='./', exclude_prolines=True, kind_analysis='sith'):
-        jedi = False
-        sith = False
-        if kind_analysis == 'sith':
-            sith = True
-        elif kind_analysis == 'jedi':
-            jedi= True
-        elif kind_analysis == 'both':
-            jedi = True
-            sith = True
-        else:
-            raise ValueError("Non-recognized kind of analysis.")
-
-        peptides = output_terminal('ls ' + data_dir, print_output=False).split('\n')
-        self.pep_infos = []
-        self.outcomes = {'jedi': [], 'sith': []}
-        self.analysis = {'jedi': [], 'sith': []}
-
-        for pep in peptides:
-            print(pep + ' ', end='')
-            if ((exclude_prolines) and ('P' in pep)) or (not isdir(f'{data_dir}/{pep}/forces')):
-                continue
-
-            self.pep_infos.append(PepSetter(f'{data_dir}/{pep}/{pep}-stretched00.pdb'))
-            
-            if jedi:
-                path = f'{data_dir}/{pep}/'
-                sith = SITH(path)
-                sith.killer(killElements='H')
-                sith.rem_first_last(from_last_minimum=True)
-                self.outcomes['jedi'].append(sith)
-                self.analysis['jedi'].append(SithAnalysis(self.outcomes['jedi'][-1],
-                                                          self.pep_infos[-1]))
-            if sith:
-                try:
-                    path = f'{data_dir}/{pep}/forces'
-                    sith = SITH(inputfiles=path)
-                    sith.rem_first_last(from_last_minimum=True)
-                    sith.sith_analysis()
-                    self.outcomes['sith'].append(sith)
-                    self.analysis['sith'].append(SithAnalysis(self.outcomes['sith'][-1],
-                                                              self.pep_infos[-1]))
-                except:
-                    print(pep)
-                    continue
-
-    def plot_le(self, a_names, aminos=3, ax: plt.Axes = None, sp=None,
-                kind_analysis='sith', lw=1, ms=1):
-        if sp is None:
-            sp = StandardPlotter()
-        if ax is None:
-            ax = sp.ax[0]
-        sp.axis_setter(ax=ax,
-                        xlabel=f'Distance({", ".join(a_names)})[A]',
-                        ylabel='Energy[Ha]')
-        for an in self.analysis[kind_analysis]:
-            l, e = an.le_dof_amino(a_names, aminos)
-            sp.plot_data(l, e, ax=ax, lw=lw, markersize=ms)
-        return ax
 
 
 def set_hes_from_ref(geo_ref, sith_tar, structure):
@@ -315,3 +253,218 @@ def set_hes_from_ref(geo_ref, sith_tar, structure):
     sith_tar.structures[structure].hessian = geo_ref.hessian
     
     return sith_tar
+
+
+class DataSetAnalysis:
+    def __init__(self, inner_steps: Callable,  data_dir: str='./',
+                 subdir='',
+                 exclude_prolines=True,
+                 exclude=None,
+                 pdb_pattern='stretched00',
+                 struc_pattern='forces*.fchk',
+                 **kwargs):
+        """
+        Creates the objects of to do the analysis of a bunch of peptides in a
+        data set.
+
+        Prameters
+        =========
+        data_dir: str or Path
+            directory containing the peptides you want to analyse. HeadUP: this
+            directory has to contain a set of directories with the name of all
+            the peptides (expected as amino code) and nothing else.
+        exclude_prolines: Bool. Default=True
+            False to include peptides with prolines.
+        pdb_pattern: str. Default='stretched00'
+            every peptide directory has to contain one and only one pdb file
+            containing the molecular information. This code will look for
+            "<data_dir>/<peptide>/*pattern*.pdb".
+        struc_pattern: str. Default='forces'
+            every peptide directory has to contain a set of files containing
+            the structure information requiered by 'sith'. This code will look
+            for files named as "<data_dir>/<peptide>/*pattern*". It will be
+            assumed that the files are organized alphabetically in the order of
+            stretched structure.
+        inner_steps: function. Default=None
+            function with the operations to apply to every sith object -its
+            unique argument- after initialized. Note that it has to have the
+            analysis you want to apply. For example,
+            ```python
+            def inner_steps(sith):
+                sith.rem_first_last(from_last_minimum=True)
+                sith.sith_analysis()
+
+                return sith
+            ```
+        **kwargs initializinf sith
+        """
+        if isinstance(data_dir, (Path, str)):
+            path = Path(data_dir)
+        assert path.is_dir(), f"{data_dir} does not exist."
+
+        peptides = list(path.glob('*/'))
+        assert len(peptides) != 0, f"There are not directories in {data_dir}"
+
+        self.pep_infos = []
+        self.names = []
+        self.outcomes = []
+        self.analysis = []
+
+        print("Log SITH analysis:\n")
+        
+        prolines = []
+        errors = []
+        if exclude is None:
+            exclude = []
+
+        for pep in peptides:
+            if (((exclude_prolines) and ('P' in pep.stem)) or (pep.stem in exclude)):
+                prolines.append(pep.stem)
+                continue
+            assert pep.is_dir(), f"{pep} does not exist."
+            
+            pdb = list(pep.glob(f'*{pdb_pattern}*.pdb'))
+            if len(pdb) == 0:
+                raise FileNotFoundError(f"Not '*{pdb_pattern}*.pdb' found in"
+                                        f"{str(pep)}")
+            elif len(pdb) > 1:
+                raise ValueError("There are more than one pdb files with the pattern"
+                                 f" {str(pep)}/*{pdb_pattern}*.pdb")
+
+            self.pep_infos.append(PepSetter(pdb[0]))
+            
+            struc = pep / subdir
+            structure_files = list(struc.glob(f'*{struc_pattern}*'))
+            structure_files.sort()
+
+            try:
+                sith = SITH(inputfiles=structure_files, **kwargs)
+                sith = inner_steps(sith)
+                sith.name = pep.stem
+                self.outcomes.append(sith)
+                self.analysis.append(SithAnalysis(self.outcomes[-1],
+                                                self.pep_infos[-1]))
+            except:
+                errors.append(pep.stem)
+
+            print(pep.stem + ' ', end='')
+        
+        print(f"\n--- A total of {len(self.outcomes)} peptides where added to the "
+              "analysis")
+    
+        if len(prolines) > 0:
+            print("--- The next peptides were neglected because they have a "
+                  "proline at least:")
+            [print(pep + ' ', end='') for pep in prolines]
+    
+        if len(errors) > 0:
+            print("\n--- The next peptides did not woked for some reason. "
+                  "Check them individually:")
+            [print(pep+ ' ', end='') for pep in prolines]
+    
+        self.test()
+    
+    def test(self):
+        """check that the first sith has the basic variables. It assumes that
+        the rest also has it"""
+        assert isinstance(self.outcomes[0].structures_scf_energies, np.ndarray)
+        assert isinstance(self.outcomes[0].dims, np.ndarray)
+        assert isinstance(self.outcomes[0].structure_energies, np.ndarray)
+        assert isinstance(self.outcomes[0].dofs_energies, np.ndarray)
+        assert isinstance(self.outcomes[0].dim_indices, np.ndarray)
+        assert isinstance(self.outcomes[0].structures, list)
+
+    def _populate_variables(self, kindanalysis):
+        """
+        This method creates the attributes used fot the analysis extracting the
+        values from the sith_objects.
+        """
+        self.all_dft_energies = []
+        self.all_edm_energies = [] # energy distribution method
+        for ed_obj in self.outcomes:
+            self.all_dft_energies.append(ed_obj.structures_scf_energies)
+            self.all_edm_energies.append(ed_obj.structure_energies)
+
+        self.all_dft_energies = np.array(self.all_dft_energies)
+        self.all_edm_energies = np.array(self.all_edm_energies)
+
+        return self
+
+    def plot_le(self, a_names, aminos=3, ax: plt.Axes = None, sp=None,
+                lw=1, ms=1, **kwargs):
+        """
+        plots the 
+        """
+        if 'ax_pref' in kwargs:
+            setter = kwargs['ax_pref']
+            del kwargs['ax_pref']
+        else:
+            setter = {}
+
+        if sp is None:
+            sp = StandardPlotter(**kwargs)
+        if ax is None:
+            ax = sp.ax[0]
+        sp.axis_setter(ax=ax,
+                        xlabel=f'Distance({", ".join(a_names)})[\u212B]',
+                        ylabel='Energy[Ha]',
+                        **setter)
+        ls = []
+        es = []
+        for an in self.analysis:
+            l, e = an.le_dof_amino(a_names, aminos)
+            ls.append(l)
+            es.append(e)
+            sp.plot_data(l, e, ax=ax, lw=lw, markersize=ms)
+        return ax, ls, es
+    
+    def le_all(self, a_names, aminos):
+        self.ls = []
+        self.es = []
+        for an in self.analysis:
+            l, e = an.le_dof_amino(a_names, aminos)
+            self.ls.append(l)
+            self.es.append(e)
+        
+
+
+    def plot_DFT_ener(self, ax: plt.Axes = None, sp=None,
+                      lw=1, ms=1, **kwargs):
+        """
+        plots the 
+        """
+        setter = kwargs['ax_pref']
+        del kwargs['ax_pref']
+
+        if sp is None:
+            sp = StandardPlotter(**kwargs)
+        if ax is None:
+            ax = sp.ax[0]
+        sp.axis_setter(ax=ax,
+                       xlabel=f'Stretched Structure',
+                       ylabel='DFT Energy[Ha]',
+                       **setter)
+        
+        xs = []
+        ys = []
+        for i, sith in enumerate(self.outcomes):
+            y = sith.structures_scf_energies
+            index1 = self.pep_infos[i].amino_info[1]['CH3'] - 1
+            index2 = self.pep_infos[i].amino_info[5]['CH3'] - 1
+            x = []
+            for struc in sith.structures:
+                dist = struc.atoms.get_distance(index1, index2)
+                x.append(dist)
+            sp.plot_data(x, y, ax=ax, lw=lw, markersize=ms)
+            xs.append(x)
+            ys.append(y)
+
+        return ax, xs, ys
+    
+    def amino_freq(self):    
+        self.names = []
+        for sith in self.outcomes:
+            self.names.append(list(sith.name))
+        self.names = np.array(self.names)
+
+        return self.names
