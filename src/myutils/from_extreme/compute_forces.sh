c34e434e86026cb0089e59f29c01920e86327de5
Sucerquia <dsucerg@gmail.com>
2024-04-25 12:05:45 +0200

    BUG: forces uses last bck chk in case of fail

--- src/myutils/from_extreme/compute_forces.sh
+++ src/myutils/from_extreme/compute_forces.sh
@@ -12,8 +12,7 @@ This tool computes the forces in all chk files and store them in a directory
 called forces.
 
     -c    run in cascade.
-    -d    directory containging the chk files of the stretching-optimization
-          process. Default ./
+    -f    <chk file> of the configuration that you want to compute the forces.
 
     -h    prints this message.
 "
@@ -22,8 +21,9 @@ exit 0
 
 compute_forces () {
     echo "construct Z-matrix for ${1%.chk}"
-    newzmat -ichk -ozmat -rebuildzmat -bmodel "$1" ${1%.chk}-forces.com || fail "
-    Error creating the matrix"
+    newzmat -ichk -ozmat -rebuildzmat -bmodel "$1" ${1%.chk}-forces.com || \
+       { lnbck=$(search_last_bck ${1%.chk}) ; \
+         newzmat -ichk -ozmat -rebuildzmat -bmodel "${1%.chk}-bck_$lnbck.chk" ${1%.chk}-forces.com || fail "creating matrix"
     sed -i "s/#P bmk\/6-31+g opt(modredun,calcfc)/%chk=${1%.chk}-forces\n%NProcShared=8\n#P bmk\/6-31+g force/g" ${1%.chk}-forces.com
     echo "executes g09 computation of forces for $1"
     g09 ${1%.chk}-forces.com || fail "computing forces"
