599b8b33719baf8c785e23b22b63739ebf443340
Sucerquia <dsucerg@gmail.com>
2024-07-02 15:01:28 +0200

    DOC: improving documentation of find_blocks

--- src/myutils/bash_scripts/find_blocks.sh
+++ src/myutils/bash_scripts/find_blocks.sh
@@ -7,17 +7,18 @@ This code extracts the sections in a file starting and finishing with specific
 patterns without including the lines containing those patterns. Check the next
 options:
 
-    -f  <file> file that shows 
-    -s  <pattern> pattern that defines the beginning of the block. This line
-        is not included in the block.
-    -e  <pattern> pattern that defines the end of the block. This line is not
-        included in the block.
-    -i  use this flag if the start and the end are indexes
-    -o  <output='output'> name of the output without extension. The output will be
-        stored in files called <output>_<n>.dat, where n is the number of
-        appearence of the block in the file.
+  -f  <file> file that shows 
+  -s  <pattern> pattern that defines the beginning of the block. This line
+      is not included in the block.
+  -e  <pattern> pattern that defines the end of the block. This line is not
+      included in the block.
+  -i  use this flag if the start and the end are indexes
+  -o  <output='output'> 'terminal' to print the output in the terminal or name
+      of the output without extension; in the later case, the output will be
+      stored in files called <output>_<n>.dat, where n is the number of
+      appearence of the block in the file.
 
-    -h  prints this message.
+  -h  prints this message.
 "
 exit 0
 }
@@ -29,20 +30,22 @@ source "$(myutils basics -path)" Find Blocks
 # ----- set up starts ---------------------------------------------------------
 # General variables
 
+starts="empty_starting_pattern"
+ends="empty_ending_pattern"
 index='false'
 output='output'
 while getopts 'e:f:io:s:h' flag;
 do
-    case "${flag}" in
-      e) ends="${OPTARG}" ;;
-      f) file="${OPTARG}" ;;
-      i) index='true' ;;
-      o) output="${OPTARG}" ;;
-      s) starts="${OPTARG}" ;;
+  case "${flag}" in
+    e) ends="${OPTARG}" ;;
+    f) file="${OPTARG}" ;;
+    i) index='true' ;;
+    o) output="${OPTARG}" ;;
+    s) starts="${OPTARG}" ;;
 
-      h) print_help ;;
-      *) echo "for usage check: myutils <function> -h" >&2 ; exit 1 ;;
-    esac
+    h) print_help ;;
+    *) echo "for usage check: myutils <function> -h" >&2 ; exit 1 ;;
+  esac
 done
 
 verbose "starts: $starts ; ends: $ends ; file: $file ; output: $output"
@@ -69,15 +72,32 @@ then
   fi
   finish
 else
-  mapfile -t nsta < <( grep -n "$starts" "$file" | \
-    awk -F ":" '{print $1}' )
+  if [[ $starts == "empty_starting_pattern" ]]
+  then
+    nsta=( 0 )
+  else
+    mapfile -t nsta < <( grep -n "$starts" "$file" | \
+      awk -F ":" '{print $1}' )
+  fi
 fi
 
 w="001"
 for (( i=0; i<${#nsta[@]}; i++ ))
 do
+  if [[ "$starts" == "$ends" ]]
+  then
+    end_line=2
+  else
+    end_line=1
+  fi
   nend=$(tail -n +"$(( ${nsta[$i]} + 1 ))" $file | grep -n "$ends" | \
-         head -n 1 | cut -d ":" -f 1)
+         head -n $end_line | cut -d ":" -f 1)
+  if [[ $empty_ending_pattern == "empty_ending_pattern" ]]
+  then
+    nend=$( tail -n +"$(( ${nsta[$i]} + 1 ))" $file | wc -l )
+    nend=$(( nend + 1 ))
+  fi
+
   if [ ${#nend} -eq 0 ]
   then
     finish
