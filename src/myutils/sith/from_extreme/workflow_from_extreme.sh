0b07ffdfc10bbf78fe118c0bac69648b8afcf50e
Sucerquia <dsucerg@gmail.com>
2024-09-17 16:10:52 +0200

    ENH: WF_from_extreme finds the length automatically

--- src/myutils/sith/from_extreme/workflow_from_extreme.sh
+++ src/myutils/sith/from_extreme/workflow_from_extreme.sh
@@ -17,14 +17,18 @@ that was just about to get a first rupture, then takes the intermedia steps and
 find the internal forces. Consider the next options:
 
   -c  run in cascade. (modules are loaded)
-  -p  <peptide>. directory or xyzfile of last conf.Chains of aminoacids to
+  -p  <peptide>. directory or xyzfile of last conf. Chains of aminoacids to
       be evaluated. For example, \"./AAA/\" would optimize the last
       stretched a trialanine peptide (where last means after organizing
       alphabetically).
   -l  <number of amino acids in the peptide> It will be assumed that the
       xyz file starts with the letter code of the amino acids.
 
+  -v  verbose.
   -h  prints this message.
+
+Note: it is assumed that the file of the last configuration is named as:
+<amino acids-code>-<description><number of stretching>.xyz
 "
 exit 0
 }
@@ -34,15 +38,14 @@ exit 0
 # ----- set up starts ---------------------------------------------------------
 cascade='false'
 ref=''
-lenght=''
-
-while getopts 'cl:p:h' flag;
+verbose='false'
+while getopts 'cl:p:vh' flag;
 do
   case "${flag}" in
     c) cascade='true' ;;
-    l) lenght=${OPTARG} ;;
     p) ref=${OPTARG} ;;
 
+    v) verbose='true' ;;
     h) print_help ;;
     *) echo "for usage check: myutils <function> -h" >&2 ; exit 1 ;;
   esac
@@ -53,13 +56,6 @@ source "$(myutils basics -path)" WF_FROM_EXTREME
 if $cascade
 then
     load_modules # ADD the parameters to resubmit
-fi
-
-# check that the variables are well set
-if [ "${#lenght}" -eq 0 ]
-then
-  fail "You have to specify the lenght of the peptide usign the flag -l. For
-    more information, use \"myutils workflow_from_extreme -h\""
 fi
 
 if [ "${#ref}" -eq 0 ]
@@ -82,13 +78,15 @@ if [ ! -f $ref ]
 then
     fail "$ref does not exist"
 fi
+
 # ---- BODY -------------------------------------------------------------------
 # ==== Optimization from extreme
 xyz=${ref##*/}
-name=${xyz:0:$lenght}
+name=${xyz%-*}
 
 verbose "The first g09 process is an optimization starting from $ref"
 
+# create from_extreme directory
 mkdir from_extreme
 cp $xyz from_extreme
 cp *00.pdb from_extreme
@@ -97,6 +95,7 @@ cd from_extreme
 # creates gaussian input that optimizes the structure
 myutils change_distance "$xyz" "$name-optext" frozen_dofs.dat 0 0 \
   "scale_distance" || fail "Preparating the input of gaussian"
+rm "$xyz"
 sed -i "1a %NProcShared=8" "$name-optext.com"
 sed -i "3a opt(modredun,calcfc)" "$name-optext.com"
 
@@ -112,7 +111,7 @@ g09 "$name-optext.com" "$name-optext.log" || \
 output=$(grep -i optimized "$name-optext.log" | \
           grep -c -i Non )
 
-[ "$output" -ne 0 ] && fail "optimization didn't converged"
+[ "$output" -ne 0 ] && fail "Optimization didn't converged"
 
 myutils after_optimization -l "$name-optext.log" -n $name
 
