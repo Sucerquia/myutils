3203bde696ad7fe3d0a4b8cc0cd7aabd90f80ad5
Sucerquia <dsucerg@gmail.com>
2025-02-06 10:48:42 +0100

    ADD: options in definition of proline DOFs

--- src/myutils/sith/change_dofs.py
+++ src/myutils/sith/change_dofs.py
@@ -19,11 +19,11 @@ def extract_dofs(indexes, atoms):
 def test_dofs(atoms, indexes, file):
     dist, angl, dihe = extract_dofs(np.array(indexes) - 1, atoms)
     old_dist = float(output_terminal(f'sed -n "/R{indexes[0]}=/p" {file}' +
-                                      ' | cut -d = -f 2', print_output=False))
+                                     ' | cut -d = -f 2', print_output=False))
     old_angl = float(output_terminal(f'sed -n "/A{indexes[0]}=/p" {file}' +
-                                      ' | cut -d = -f 2', print_output=False))
+                                     ' | cut -d = -f 2', print_output=False))
     old_dihe = float(output_terminal(f'sed -n "/D{indexes[0]}=/p" {file}' +
-                                      ' | cut -d = -f 2', print_output=False))
+                                     ' | cut -d = -f 2', print_output=False))
 
     assert old_dist == approx(dist, abs=3e-2), f"R{indexes[0]}({old_dist}) does not " + \
         f"correspond to the expected from the xyz file({dist})"
@@ -38,25 +38,103 @@ def def_line(indexes, element):
     return f'{element},{a2},R{a1},{a3},A{a1},{a4},D{a1}'
 
 
-def change_def(old_i, new_i, element, atoms, file):
+def change_def(new_i, element, atoms, file):
+    # All the atoms used to define a new atom should already exist; they
+    # should have lower index.
+    for i in new_i[1:]:
+        if new_i[0] < i:
+            print("At least two atoms have to be swaped. You are trying to " +
+                  f"redefine {new_i}.")
+
     # Create new and old lines
-    old_line = def_line(old_i, element)
     new_line = def_line(new_i, element)
 
     # Find variables
+    tochange = new_i[0]
     new_i = np.array(new_i) - 1
-    old_i = np.array(old_i) - 1
     dist, angl, dihe = extract_dofs(new_i, atoms)
-    
+
     # Change value
-    output_terminal(f'sed -i "s/{old_line}/{new_line}/g" {file}')
-    output_terminal(f'sed -i "/R{old_i[0]}=/c\ R{old_i[0]}={dist}" {file}')
-    output_terminal(f'sed -i "/A{old_i[0]}=/c\ A{old_i[0]}={angl}" {file}')
-    output_terminal(f'sed -i "/D{old_i[0]}=/c\ D{old_i[0]}={dihe}" {file}')
+    output_terminal(f'sed -i "s/,R{tochange},/c{new_line}/g" {file}')
+    output_terminal(f'sed -i "/R{tochange}=/c\ R{tochange}={dist}" {file}')
+    output_terminal(f'sed -i "/A{tochange}=/c\ A{tochange}={angl}" {file}')
+    output_terminal(f'sed -i "/D{tochange}=/c\ D{tochange}={dihe}" {file}')
+
+
+def find_CAC_NC(amino_info, atoms, i_pro):
+    """
+    Finds the C closest to the Ca atom and to the N atom. This is necessary
+    because C could be in the same, the previous or in the next amino.
+
+    Parameters
+    ==========
+    amino_info: dict
+       dictionary with the amino info
+    atoms: ase.Atoms
+        atoms of a given configuration.
+    i_pro: int
+        index of the proline residue.
+
+    Returns
+    =======
+    (tuple), index of C closest to Ca and index of the C closest to N.
+    """
+    pre_amino = amino_info[i_pro - 1]
+    amino = amino_info[i_pro]
+    post_amino = amino_info[i_pro + 1]
+
+    # obtain indices
+    c_pre = pre_amino['C'] - 1
+    c_cur = amino['C'] - 1
+    c_pos = post_amino['C'] - 1
+    n_i = amino['CA'] - 1
+    ca_i = amino['N'] - 1
+
+    # find Ca-C and N-C
+    ca_dist = 100
+    n_dist = 100
+    cac = 0
+    cn = 0
+    for i in [c_pre, c_cur, c_pos]:
+        # Ca-C
+        d = atoms.get_distance(i, ca_i)
+        if d < ca_dist:
+            ca_dist = d
+            cac = i 
+        # C-N
+        d = atoms.get_distance(i, n_i)
+        if d < n_dist:
+            n_dist = d
+            cn = i
+
+    return cac, cn
 
 
 # add2executable
-def change_prolines_dofs(comfile, molecule, pdb_template):
+def change_prolines_dofs(comfile, molecule, pdb_template, option):
+    """
+    Changes the definition of the degrees of freedom in prolines such that the
+    missed bond is always different.
+
+    Parameters
+    ==========
+    comfile: str
+        g09 input comfile that you want to modify.
+    molecule: str
+        xyz file representing the molecule.
+    pdb_template: str
+        pdb with the peptide information.
+    option: str
+        '1' misses Cgamma Cdelta.
+        '2' misses Cbeta Cgamma.
+        '3' misses Calpha Cbeta.
+        '4' misses Cdelta N.
+
+    Returns
+    =======
+    (None) However, it changes the comfile. Be sure to back up the original
+    file.
+    """
     pep_set = PepSetter(pdb_template)
     atoms = read(molecule)
     pros_i = np.where(np.array(list(pep_set.amino_name.values())) == 'PRO ')[0] + 1
@@ -65,36 +143,109 @@ def change_prolines_dofs(comfile, molecule, pdb_template):
         amino = pep_set.amino_info[i_pro]
         Ca_i = amino['CA']
         Cb_i = amino['CB']
+        HB1_i = amino['2HB']
+        HB2_i = amino['3HB']
         Cg_i = amino['CG']
-        Cd_i = amino['CD']
         HG1_i = amino['2HG']
         HG2_i = amino['3HG']
+        Cd_i = amino['CD']
+        HD1_i = amino['2HD']
+        HD2_i = amino['3HD']
         N_i = amino['N']
+        CCA_i, CN_i = find_CAC_NC(pep_set.amino_info, atoms, i_pro)
 
         # test that xyz does does correspond to the com file
-        test_dofs(atoms,
-                  np.array([Cg_i, Cb_i, Ca_i, N_i]),
-                  comfile)
-        test_dofs(atoms,
-                  np.array([HG1_i, Cg_i, Cb_i, Ca_i]),
-                  comfile)
-        test_dofs(atoms,
-                  np.array([HG2_i, Cg_i, Cb_i, HG1_i]),
-                  comfile)
+        test_dofs(atoms, np.array([Cg_i, Cb_i, Ca_i, N_i]), comfile)
+        test_dofs(atoms, np.array([HG1_i, Cg_i, Cb_i, Ca_i]), comfile)
+        test_dofs(atoms, np.array([HG2_i, Cg_i, Cb_i, HG1_i]), comfile)
 
-        # change Cg dofs
-        change_def(np.array([Cg_i, Cb_i, Ca_i, N_i]),
-                   np.array([Cg_i, Cd_i, N_i, Ca_i]),
-                   'C', atoms, comfile)
+        # option 1 (Default)
+        set1(Ca_i, Cb_i, HB1_i, HB2_i, Cg_i, HG1_i, HG2_i, Cd_i, HD1_i,
+                HD2_i, N_i, CCA_i, CN_i, atoms, comfile)
 
-        # change Gg1 dofs
-        change_def(np.array([HG1_i, Cg_i, Cb_i, Ca_i]),
-                   np.array([HG1_i, Cg_i, Cd_i, N_i]),
-                   'H', atoms, comfile)
-
-        # change Gg1 dofs
-        change_def(np.array([HG2_i, Cg_i, Cb_i, HG1_i]),
-                   np.array([HG2_i, Cg_i, Cd_i, N_i]),
-                   'H', atoms, comfile)
+        if option == '2':
+            # only changes Cgamma
+            set2(Ca_i, Cb_i, Cg_i, Cd_i, HG1_i, HG2_i, N_i, atoms, comfile)
         
-        output_terminal(f'myutils switch_atoms_in_com {Cg_i} {Cd_i} {comfile}')
+        if option == '3':
+            # First change Cgamma
+            set2(Ca_i, Cb_i, Cg_i, Cd_i, HG1_i, HG2_i, N_i, atoms, comfile)
+            # and then change Cbeta
+            set3()
+
+        if option == '4':
+            # Only changes Cdelta
+            set4(Cd_i, Cg_i, Cb_i, Ca_i, HD1_i, HD2_i, atoms, comfile)
+
+
+def set1(Ca_i, Cb_i, HB1_i, HB2_i, Cg_i, HG1_i, HG2_i, Cd_i, HD1_i,
+            HD2_i, N_i, CCA_i, CN_i, atoms, comfile):
+
+    # change betas dofs
+    change_def(np.array([Cb_i, Ca_i, N_i, CN_i]),
+               'C', atoms, comfile)
+    change_def(np.array([HB1_i, Cb_i, Ca_i, N_i]),
+               'H', atoms, comfile)
+    change_def(np.array([HB2_i, Cb_i, Ca_i, N_i]),
+               'H', atoms, comfile)
+
+    # change gammas dofs
+    change_def(np.array([Cg_i, Cb_i, Ca_i, N_i]),
+               'C', atoms, comfile)
+    change_def(np.array([HG1_i, Cg_i, Cb_i, Ca_i]),
+               'H', atoms, comfile)
+    change_def(np.array([HG2_i, Cg_i, Cb_i, Ca_i]),
+               'H', atoms, comfile)
+
+    # change deltas dofs
+    change_def(np.array([Cd_i, N_i, Ca_i, CCA_i]),
+               'C', atoms, comfile)
+    change_def(np.array([HD1_i, Cd_i, N_i, Ca_i]),
+               'H', atoms, comfile)
+    change_def(np.array([HD2_i, Cd_i, N_i, Ca_i]),
+               'H', atoms, comfile)
+
+
+def set2(Cg_i, Cd_i, N_i, Ca_i, HG1_i, HG2_i, atoms, comfile):
+    # change Cg dofs
+    change_def(np.array([Cg_i, Cd_i, N_i, Ca_i]),
+               'C', atoms, comfile)
+
+    # change Gg1 dofs
+    change_def(np.array([HG1_i, Cg_i, Cd_i, N_i]),
+               'H', atoms, comfile)
+
+    # change Gg1 dofs
+    change_def(np.array([HG2_i, Cg_i, Cd_i, N_i]),
+               'H', atoms, comfile)
+    output_terminal(f'myutils switch_atoms_in_com {Cg_i} {Cd_i} {comfile}')
+
+def set3(Cb_i, Cg_i, Cd_i, N_i, HB1_i, HB2_i, atoms, comfile):
+    # change Cg dofs
+    change_def(np.array([Cb_i, Cg_i, Cd_i, N_i]),
+               'C', atoms, comfile)
+
+    # change Gg1 dofs
+    change_def(np.array([HB1_i, Cb_i, Cg_i, Cd_i]),
+               'H', atoms, comfile)
+
+    # change Gg1 dofs
+    change_def(np.array([HB2_i, Cb_i, Cg_i, Cd_i]),
+               'H', atoms, comfile)
+
+    output_terminal(f'myutils switch_atoms_in_com {Cb_i} {Cg_i} {comfile}')
+    output_terminal(f'myutils switch_atoms_in_com {Cg_i} {Cd_i} {comfile}')
+
+
+def set4(Cd_i, Cg_i, Cb_i, Ca_i, HD1_i, HD2_i, atoms, comfile):
+    # change Cg dofs
+    change_def(np.array([Cd_i, Cg_i, Cb_i, Ca_i]),
+               'C', atoms, comfile)
+
+    # change Gg1 dofs
+    change_def(np.array([HD1_i, Cd_i, Cg_i, Cb_i]),
+               'H', atoms, comfile)
+
+    # change Gg1 dofs
+    change_def(np.array([HD2_i, Cd_i, Cg_i, Cb_i]),
+               'H', atoms, comfile)
