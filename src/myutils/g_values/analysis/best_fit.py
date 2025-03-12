c8faa6647ea8e3258b04054441e992530ae76969
Sucerquia <danielsucerquia@hotmail.com>
2025-03-12 11:43:47 +0100

    ADD: fitter of spectrum after compute gvals

--- src/myutils/g_values/analysis/best_fit.py
+++ src/myutils/g_values/analysis/best_fit.py
@@ -1,5 +1,103 @@
 import numpy as np
 from scipy.optimize import nnls
+from myutils.miscellaneous import output_terminal
+
+
+class TheoMatchExpe:
+    """
+    Collects the computed data and tries to fit it to the experiment spectrum
+
+    Parameters
+    ==========
+    experiment_file: str. Default='../experiments/field_vs_spectrum.dat'
+        .dat field vs intensity of the experiment.
+    basis_pattern: str. Default='spectrum_wo_hyFiCorr.dat'
+        pattern of the .dat files containing the spectrum of the computed spectrum.
+    basis_location: str. Default='../'
+        location where to search for the files with the basis_pattern.
+
+    
+    Note
+    ====
+    This script assumes that the basis files has a common pattern and they are all
+    in subdirectories of the location where it is running. It also assumes that all
+    the values of the computed intensities are defined for the same values of the field.
+    """
+    def __init__(self,
+                 files,
+                 experiment_file='../experiments/field_vs_spectrum2024.dat'):
+    
+        self.fieldexp, self.intensexp = np.loadtxt(experiment_file, unpack=True)
+        self.files = np.array(files)
+
+        self.field = np.loadtxt(self.files[0], usecols=0)
+        self.intensities = []
+        for file in self.files:
+            intensity = np.loadtxt(file, usecols=1)
+            self.intensities.append(intensity)
+        self.intensities = np.array(self.intensities)
+
+        # Interpolate and find coeffs
+        A = np.column_stack([np.interp(self.fieldexp,
+                                       self.field, bf) for bf in self.intensities])
+        self.coeffs = nnls(A, self.intensexp)[0].reshape(len(self.intensities), 1)
+        self.intensfit = np.sum(self.coeffs * self.intensities, axis=0)
+        self.proportions()
+
+    
+    def proportions(self, print_analysis=False):
+        """
+        Computes the percentage of each one of the candidates that fit better
+        the experimental spectrum.
+
+        Parameters
+        ==========
+        print_analysis: Bool. Default=False
+            True to print the percentage of each one of the candidates.
+        
+        Return
+        ======
+        (np.array) percentages.
+        """
+        total = np.sum(self.coeffs)
+        self.percentages = 100 * self.coeffs.flatten() / total
+        if print_analysis:
+            for i, name in enumerate(self.files):
+                print(name, " (%): ", self.percentages[i])
+
+        return self.percentages
+    
+    def clean_candidates(self, threshold=1):
+        """
+        Removes the candidates that are expected to be less than certain
+        percentage. It means, TheoFitExpe.intensities and TheoFitExpe.files
+        might be reduced.
+
+        Parameters
+        ==========
+        threshold: float. Default=1
+            minimum percentage that a candidate should have to be considered.
+
+        Return
+        ======
+        (np.array) new set of intensities.
+        """
+        self.proportions()
+        condition = self.percentages >= threshold
+        if condition.all():
+            return self.intensities
+
+        self.files = self.files[condition]
+        self.intensities = self.intensities[condition]
+
+        # refitting
+        A = np.column_stack([np.interp(self.fieldexp,
+                                       self.field, bf) for bf in self.intensities])
+        
+        self.coeffs = nnls(A, self.intensexp)[0].reshape(len(self.intensities), 1)
+        self.intensfit = np.sum(self.coeffs * self.intensities, axis=0)
+        self.proportions()
+        self.clean_candidates(threshold=threshold)
 
 
 def fit_experiment(experiment_file='../experiments/field_vs_spectrum.dat',
@@ -25,7 +123,7 @@ def fit_experiment(experiment_file='../experiments/field_vs_spectrum.dat',
     the values of the computed intensities are defined for the same values of the field.
     """
     fieldexp, intensexp = np.loadtxt(experiment_file, unpack=True)
-    files = output_terminal(f"find ../ -name '${basis_pattern}'", print_output=False)
+    files = output_terminal(f"find ../ -name '${basis_pattern}' | sort", print_output=False)
     files = files.split('\n')[:-1]
 
     field = np.loadtxt(files[0], usecols=0)
@@ -45,8 +143,10 @@ def fit_experiment(experiment_file='../experiments/field_vs_spectrum.dat',
 
 
 # add2executable
-def create_fit_file(output, experiment_file='../experiments/field_vs_spectrum.dat',
-                    basis_pattern='spectrum_wo_hyFiCorr.dat'):
+def create_fit_file(output,
+                    experiment_file='../experiments/field_vs_spectrum.dat',
+                    basis_pattern='spectrum_wo_hyFiCorr.dat',
+                    basis_location='../'):
     """
     Creates a .dat file with field vs intensity of the best fit to an
     experiment with some given calculated spectrums.
@@ -59,10 +159,12 @@ def create_fit_file(output, experiment_file='../experiments/field_vs_spectrum.da
         .dat field vs intensity of the experiment.
     basis_pattern: str. Default='spectrum_wo_hyFiCorr.dat'
         pattern of the .dat files containing the spectrum of the computed spectrum.
+    basis_location: str. Default='../'
+        location where to search for the files with the basis_pattern.
     
     Return
     ======
-    (None) it does not return anything but stores the dat in a given file.
+    (None) it does not return anything but creates the .dat file with the fit.
 
     Note
     ====
@@ -70,7 +172,12 @@ def create_fit_file(output, experiment_file='../experiments/field_vs_spectrum.da
     in subdirectories of the location where it is running. It also assumes that all
     the values of the computed intensities are defined for the same values of the field.
     """
-    _, _ , field, inten = fit_experiment(experiment_file, basis_pattern)
+    files = output_terminal(f"find {basis_location} -name '${basis_pattern}'", print_output=False)
+    files = files.split('\n')[:-1]
+    tme = TheoMatchExpe(files, experiment_file)
+    field = tme.field
+    inten = tme.intensfit
+
     with open(output, 'w') as outfile:
         outfile.write('# field intensity\n')
         for f, i in zip(field, inten):
