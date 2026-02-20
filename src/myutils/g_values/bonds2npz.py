a6efc87c139c693c58fd75ac78b86841fe76a0f6
Sucerquia <danielsucerquia@hotmail.com>
2026-02-20 18:03:05 +0100

    second trial to add bonds
    this time it failed because xyz does not have the same orientation than
    the original H xyz

--- src/myutils/g_values/bonds2npz.py
+++ src/myutils/g_values/bonds2npz.py
@@ -1,6 +1,8 @@
 from myutils.g_values.g_valsetup import ext_xyz_from_npz, get_connectivity
 import numpy as np
 import glob
+from ase import Atoms
+from ase.io import write
 
 
 # add2executable
@@ -22,45 +24,59 @@ def bonds2npz(npz_file):
     """
     if npz_file.endswith('.npz'):
         npz_files = [npz_file]
-    if npz_file.endswith('.dat'):
+    elif npz_file.endswith('.dat'):
         npz_files = np.loadtxt(npz_file, dtype=str)
     else:
         npz_files = glob.glob(npz_file + '/*.npz')
 
     for npz_file in npz_files:
         print(f'{npz_file}')
-        molecules = ext_xyz_from_npz(npz_file, create_xyz_files=False)
-        connectivity = []
+        data = np.load(npz_file)
+        H = Atoms(symbols='H', positions=[data['hydrogen_xyz'][0]])
+        radical = ext_xyz_from_npz(npz_file, create_xyz_files=False)[0]
+        molecule = radical[:data['original_hydrogen_idxs'][0]] + H + \
+            radical[data['original_hydrogen_idxs'][0]:]
+        con = get_connectivity(molecule, 'vmd')
+        if len(con[data['original_hydrogen_idxs'][0]]) != 1:
+            write(f'totest.xyz', molecule)
+            raise ValueError(f'Original hydrogen {data["original_hydrogen_idxs"][0]} ' +\
+                f'should have only one bond. Got {len(con[data["original_hydrogen_idxs"][0]])}.')
+            
+        unique = []
+        for i, connections in enumerate(con):
+            for j in connections:
+                if j > i:
+                    unique.append([i, j])
+        nb_ori = len(unique)
 
-        for k, atoms in enumerate(molecules):
-            con = get_connectivity(atoms, 'vmd')
+        connectivity = []
+        for hi in data['original_hydrogen_idxs']:
             unique = []
             for i, connections in enumerate(con):
+                if i == hi:
+                    continue
+                elif i < hi:
+                    l = i
+                else:
+                    l = i - 1
                 for j in connections:
+                    if j == hi:
+                        continue
+                    elif j < hi:
+                        m = j
+                    else:
+                        m = j - 1
                     if j > i:
-                        unique.append([i, j])
+                        unique.append([l, m])
+            assert len(unique) == nb_ori - 1, f'Number of bonds for original ' + \
+                f"hydrogen {hi} is not correct. Expected {nb_ori - 1}, " +\
+                f"got {len(unique)}. {len(unique), nb_ori - 1}"
             connectivity.append(unique)
-
-        n_bonds_per_mol = [len(b) for b in connectivity]
-        values, counts = np.unique(n_bonds_per_mol, return_counts=True)
-        mode = values[np.argmax(counts)]
-        off = []
-        for i, n in enumerate(n_bonds_per_mol):
-            if n != mode:
-                off.append(i)
-
-        for k, bonds in enumerate(connectivity):
-            with open(npz_file[:-4] + f'_{k:03d}_bonds.dat', 'w') as f:
-                if k in off:
-                    f.write(f'# This molecule has {len(bonds)} bonds, which is different from the mode of {mode} bonds.\n')
-
-                for bond in bonds:
-                    f.write(f'{bond[0]} {bond[1]}\n')
 
         data = np.load(npz_file)
         data = {key: data[key] for key in data.files}
-        #data['bonds'] = np.array(connectivity)
-        #np.savez(npz_file, **data)
+        data['bonds'] = np.array(connectivity)
+        np.savez(npz_file, **data)
 
     return connectivity
 
